8f77ad549478ab3b70051fbfaad12ac4ec273ea6
trevortega <trevor.ortega.12@gmail.com>
2022-04-24 22:04:21 -0700

    Lazy-load from cache + cache file

--- app/controllers/posts.controllers.js
+++ app/controllers/posts.controllers.js
@@ -1,18 +1,22 @@
 const Post = require("../models/post");
-
+const redis = require('./models/cache')
 
 const homeStartingContent =
 	'The home pages lists all the blogs from all the users.';
 
 const composePost = (req, res) => {
 	const post = new Post({
-    username: req.user.username,
+    	username: req.user.username,
 		title: req.body.postTitle,
 		content: req.body.postBody
 	});
 
-  redis.mset(post) 
 	post.save();
+
+	/*
+	const id = postgres.insert(post);
+	redis.set(id, post);
+	*/
 	res.redirect('/post');
 };
 
@@ -29,12 +33,25 @@ const displayAllPosts = (req, res) => {
 async function displayPost (req, res)  {
 	const requestedPostId = req.params.postId;
 
-	Post.findOne({ _id: requestedPostId }, function(err, post) {
-		res.render('post', {
-			title: post.title,
-			content: post.content
-		});
-	});
+	redis.get(requestedPostId, (err, result) => {
+		if (err) {
+			Post.findOne({ _id: requestedPostId }, function(err, post) {
+				redis.set(requestedPostId, JSON.stringify(post));
+				res.render('post', {
+					title: post.title,
+					content: post.content
+				});
+			});
+		} else {
+			post = JSON.parse(result);
+			res.render('post', {
+				title: result.title,
+				content: result.content
+			});
+		}
+	  });
+
+	
 };
 
 module.exports = {
