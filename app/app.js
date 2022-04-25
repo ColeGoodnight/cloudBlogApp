398f3b1aea9a193566081a91e7de284cc1bb0250
trevortega <trevor.ortega.12@gmail.com>
2022-04-24 22:04:21 -0700

    Lazy-load from cache + cache file

--- app/app.js
+++ app/app.js
@@ -90,14 +90,6 @@ sequelize
 
 
 
-let redis = new Redis({
-  port: 6379, // Redis port
-  host: "127.0.0.1", // Redis host
-  username: "awsTime", // needs Redis >= 6
-  password: "pleaseWorkAWS",
-  db: 0, // Defaults to 0
-});
-
 app.use(
 	session({
 		secret: config.get('secret'),
