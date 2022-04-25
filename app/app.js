92db6dbf705d72843132c069759dbe2c7f2eb0a0
TrevORtega <38680575+TrevORtega@users.noreply.github.com>
2022-04-24 19:49:07 -0700

    First steps redis

--- app/app.js
+++ app/app.js
@@ -4,7 +4,9 @@ const config = require('./config/config');
 const compression = require ('compression');
 const helmet = require('helmet');
 const https= require("https");
-const fs = require('fs');
+const fs = require('fs')
+
+const Redis = require("ioredis");
 
 // Global cache
 const redis = require('./models/cache')
@@ -87,6 +89,14 @@ sequelize
 });*/
 
 
+
+let redis = new Redis({
+  port: 6379, // Redis port
+  host: "127.0.0.1", // Redis host
+  username: "awsTime", // needs Redis >= 6
+  password: "pleaseWorkAWS",
+  db: 0, // Defaults to 0
+});
 
 app.use(
 	session({
